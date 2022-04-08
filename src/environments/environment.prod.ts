f47be2a1789d9475ccd1f457d770e6f49f837b50
Stu88orn <marc.ptas@gmail.com>
2022-04-08 20:44:45 +0200

    1.0.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: "https://airlabs.co/api/v9/",
-  api_KEY: "24b3cf3c-62f3-481c-afb1-a087a136c642"
+  api_KEY: "24b3cf3c-62f3-481c-afb1-a087a136c642",
+
+  apiUrlWeather: 'http://api.openweathermap.org/data/2.5/weather?q=',
+  apiKeyWeather: 'b03ccc34733587e4577032f8b5796315'
 };
