2573f73c59dbf3b29f552f1bdc6ede7ec2cc2a9a
Mateusz Cieślik <mateuszcieslik@gmail.com>
2022-04-10 13:55:37 +0200

    airbus

--- src/assets/scripts/table.js
+++ src/assets/scripts/table.js
@@ -1,3 +1,27 @@
 function greet(){
   alert("Hello");
 }
+
+const oneSeat = document.querySelectorAll('.oneSeat');
+const seat = document.querySelectorAll('.seat');
+const active = document.querySelectorAll('.active');
+const label = document.querySelectorAll('label');
+const input = document.querySelectorAll('input');
+
+
+oneSeat.forEach( seat => seat.addEventListener('click', freeSeat));
+
+
+for (let i = 0; i < oneSeat.length; i++) {
+    oneSeat[i].addEventListener('click', function(e){
+        if(e.target.classList.contains('active')){
+            e.target.classList.remove('active');
+            input[i].checked = false;
+            label[i].classList.remove('active');
+        } else {
+            e.target.classList.add('active');
+            input[i].checked = true;
+            label[i].classList.add('active');
+        }
+    });
+}
