f70ae7517f0e3ca1c9a83390960e5fefc9190382
Marcin <89456997+Stu88orn@users.noreply.github.com>
2022-05-29 18:36:54 +0200

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "https://airlabs.co/api/v9/",
-  apiKEY: "api_key=7c9ec442-36a6-4602-9128-6da82e48ba42",
-
-  apiUrlWeather: 'http://api.openweathermap.org/data/2.5/weather?q=',
-  apiKeyWeather: 'b03ccc34733587e4577032f8b5796315'
-};
